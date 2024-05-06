8437a485f226f082c8b2a0fbca3a6f0ec9da9452
likith1003 <123715227+likith1003@users.noreply.github.com>
2024-05-06 10:00:01 +0530

    Update models.py

--- project6/app/models.py
+++ project6/app/models.py
@@ -11,4 +11,7 @@ class Register(models.Model):
     cource = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
     
